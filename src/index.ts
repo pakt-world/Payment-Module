f10cff9f8abc25402cc365738d5139ae5b8148fe
beardkoda <akinsuyi.joshua84@gmail.com>
2025-01-17 08:46:36 +0200

    fix: project setup

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,3 @@
-//  Author: Paul Adedokun for Pakt (https://github.com/Paul-Taiwo)
+//  Author: BeardKoda for Pakt (https://github.com/BeardKoda)
 
 export * from "./context/config-context";
-export { default as ReportModal } from "./components/report-modal";
-export { default as DefendantModal } from "components/defendant-modal";
-export { default as JuryModal } from "components/jury-modal";
-export { default as ResultsModal } from "components/results-modal";
