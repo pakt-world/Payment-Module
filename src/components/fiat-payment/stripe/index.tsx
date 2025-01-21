20619da42742a98a57e9909efb1f47ca409c2046
beardkoda <akinsuyi.joshua84@gmail.com>
2025-01-21 21:41:59 +0200

    feat: initial stripe integration

--- src/components/fiat-payment/stripe/index.tsx
+++ src/components/fiat-payment/stripe/index.tsx
@@ -12,20 +12,25 @@ import { useState } from 'react';
 
 import { CryptoElements, OnrampElement } from './context';
 import { StripeModalProps } from './type';
+import Modal from 'components/common/modal';
 
 // const LOADING_STATE = "loading";
 // const ERROR_STATE = "error";
 
-const StripePaymentModal = ({  }:StripeModalProps) => {
-  const [clientSecret, setClientSecret] = useState<string>("");
-  const stripeOnrampPromise = loadStripeOnramp("pk_test_qblFNYngBkEdjEZ16jxxoWSM");
-
-  // if 
+const StripePaymentModal = ({ isOpen, closeModal, publicKey, amount, chain, coin, depositAddress, secretKey }:StripeModalProps) => {
+  const [clientSecret, setClientSecret] = useState<string>(secretKey);
+  const stripeOnrampPromise = loadStripeOnramp(publicKey);
   
   return(
-    <CryptoElements stripeOnramp={stripeOnrampPromise}>
-      <OnrampElement clientSecret={clientSecret} />
-    </CryptoElements>
+    <Modal 
+      isOpen={isOpen}
+      closeModal={closeModal}
+      disableClickOutside
+    >
+      <CryptoElements stripeOnramp={stripeOnrampPromise}>
+        <OnrampElement clientSecret={clientSecret} />
+      </CryptoElements>
+    </Modal>
   )
 }
 
